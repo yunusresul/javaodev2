a2ae66e05df01eff85b9d399b6c06b61680abdcf
yunusresul <yunusresulozkan55@gmail.com>
2022-09-28 19:38:21 +0300

    javakampodev2

---
+++ javaodev2022/sayiBulma/src/sayiBulma/Main.java
@@ -0,0 +1,29 @@
+package sayiBulma;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] sayilar = new int[] {1,2,5,7,9,0};
+		int aranacak = 6;
+		boolean varMi = false;
+		for(int sayi : sayilar)
+		{
+			if(aranacak == sayi)
+			{
+				varMi = true;
+				break;
+			}
+		}
+		if(varMi) {
+			System.out.println("Sayı mevcuttur");
+		}
+		else {
+			System.out.println("Sayı mevcut değildir.");
+		}
+		
+
+	}
+
+}
